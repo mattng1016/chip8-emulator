01c187eb6cb62a4f8bb822707f15a6f2fe724817
mattng1016 <mattng1016@gmail.com>
2026-09-14 15:35:37 -0700

    comparison instructions and jump return

--- emulator.c
+++ emulator.c
@@ -7,7 +7,7 @@
 
 typedef struct {
   uint8_t memory[4096];
-  uint8_t registers[16];
+  uint8_t V[16];
   uint8_t keyboard[16];
   uint32_t display[64][32];
   uint8_t delayTimer;
@@ -41,7 +41,171 @@ void opcode_00E0(Chip8* chip8, int16_t opcode) {
   memset(chip8->display, 0, sizeof(chip8->display)); 
 }
 
+// Returns from subroutine
 void opcode_00EE(Chip8* chip8, int16_t opcode) {
+  chip8->programCounter = chip8->stack[chip8->stackPointer];
+  chip8->stackPointer--;
+}
+
+// Jump to location nnn
+void opcode_1nnn(Chip8* chip8, int16_t opcode) {
+  uint16_t address = opcode & 0xFFF;
+  chip8->programCounter = address;
+}
+
+// Call subroutine at nnn
+void opcode_2nnn(Chip8* chip8, int16_t opcode) {
+  chip8->stackPointer++;
+  chip8->stack[chip8->stackPointer] = chip8->programCounter;
+  chip8->programCounter = opcode & 0xFFF;
+}
+
+// Skip next instruction if Vx == kk
+void opcode_3xkk(Chip8* chip8, int16_t opcode) {
+  uint16_t x = (opcode >> 8) & 0xF;
+  uint16_t kk = opcode & 0xFF;
+  if (chip8->V[x] == kk) {
+    chip8->programCounter += 2;
+  }
+}
+
+// Skip next instruction if Vx != kk
+void opcode_4xkk(Chip8* chip8, int16_t opcode) {
+  uint16_t x = (opcode >> 8) & 0xF;
+  uint16_t kk = opcode & 0xFF;
+  if (chip8->V[x] != kk) {
+    chip8->programCounter += 2;
+  }
+}
+
+// Skip next instruction if Vx == Vy
+void opcode_5xy0(Chip8* chip8, int16_t opcode) {
+  uint16_t x = (opcode >> 8) & 0xF;
+  uint16_t y = (opcode >> 4) & 0xF;
+  if (chip8->V[x] == chip8->V[y]) {
+    chip8->programCounter += 2;
+  }
+}
+
+void opcode_6xkk(Chip8* chip8, int16_t opcode) {
+
+}
+
+void opcode_7xkk(Chip8* chip8, int16_t opcode) {
+
+}
+
+void opcode_8xy0(Chip8* chip8, int16_t opcode) {
+
+}
+
+void opcode_8xy1(Chip8* chip8, int16_t opcode) {
+
+}
+
+
+void opcode_8xy2(Chip8* chip8, int16_t opcode) {
+
+}
+
+void opcode_8xy3(Chip8* chip8, int16_t opcode) {
+
+}
+
+void opcode_8xy4(Chip8* chip8, int16_t opcode) {
+
+}
+
+void opcode_8xy5(Chip8* chip8, int16_t opcode) {
+
+}
+
+void opcode_8xy6(Chip8* chip8, int16_t opcode) {
+
+}
+
+void opcode_8xy7(Chip8* chip8, int16_t opcode) {
+
+}
+
+void opcode_8xyE(Chip8* chip8, int16_t opcode) {
+
+}
+
+void opcode_9xy0(Chip8* chip8, int16_t opcode) {
+
+}
+
+void opcode_Annn(Chip8* chip8, int16_t opcode) {
+
+}
+
+void opcode_Bnnn(Chip8* chip8, int16_t opcode) {
+
+}
+
+void opcode_Cxkk(Chip8* chip8, int16_t opcode) {
+
+}
+
+
+void opcode_Dxyn(Chip8* chip8, int16_t opcode) {
+
+}
+
+
+void opcode_Ex9E(Chip8* chip8, int16_t opcode) {
+
+}
+
+
+void opcode_ExA1(Chip8* chip8, int16_t opcode) {
+
+}
+
+
+void opcode_Fx07(Chip8* chip8, int16_t opcode) {
+
+}
+
+
+void opcode_Fx0A(Chip8* chip8, int16_t opcode) {
+
+}
+
+
+void opcode_Fx15(Chip8* chip8, int16_t opcode) {
+
+}
+
+
+void opcode_Fx18(Chip8* chip8, int16_t opcode) {
+
+}
+
+
+void opcode_Fx1E(Chip8* chip8, int16_t opcode) {
+
+}
+
+
+void opcode_Fx29(Chip8* chip8, int16_t opcode) {
+
+}
+
+
+void opcode_Fx33(Chip8* chip8, int16_t opcode) {
+
+}
+
+
+void opcode_Fx55(Chip8* chip8, int16_t opcode) {
+
+}
+
+
+void opcode_Fx65(Chip8* chip8, int16_t opcode) {
+
 }
 
 // Emulate a CPU cycle
@@ -50,6 +214,11 @@ void cycle(Chip8* chip8) {
   uint16_t opcode = (chip8->memory[chip8->programCounter] << 8) | chip8->memory[(chip8->programCounter) + 1];
   chip8->programCounter += 2;
   // Decode
+  printf("%x\n", chip8->programCounter);
+  chip8->V[0xB] = 0xA;
+  chip8->V[0xC] = 0xA;
+  opcode_5xy0(chip8, 0xABA0);
+  printf("%x", chip8->programCounter);
   // Execute
 }
 
