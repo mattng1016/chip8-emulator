20f9f50a283f8fa0e8bcfcb4fadbd60a1f4410bf
mattng1016 <mattng1016@gmail.com>
2026-09-14 12:04:21 -0700

    load rom function

--- emulator.c
+++ emulator.c
@@ -1,4 +1,6 @@
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct {
   uint8_t memory[4096];
@@ -12,3 +14,38 @@ typedef struct {
   uint16_t stack[16];
   uint16_t registerI;
 } Chip8;
+
+void init(Chip8* chip) {
+  chip->programCounter = 0x200;
+
+}
+
+// Loads ROM from given path into Chip8
+void loadROM(Chip8* chip, char* fileName) {
+  FILE* rom = fopen(fileName, "rb"); 
+  if (!rom) {
+    printf("Error loading ROM: %s\n", fileName);
+    exit(1);
+  }
+  fread(&chip->memory[0x200], 1, sizeof(chip->memory), rom);
+  fclose(rom);
+}
+
+// Debug
+void printROM(Chip8* chip) {
+  for (int i = 0; i < sizeof(chip->memory); i++) {
+    printf("%d", chip->memory[i+0x200]);
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  if (argc != 2 ) {
+    printf("Error: Usage: ./emulator <ROM path>\n");
+    exit(1);
+  }
+
+  Chip8 c;
+  loadROM(&c, argv[1]);
+  return 1;
+}
